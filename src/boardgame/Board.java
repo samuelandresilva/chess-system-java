56c380b4342bf786a01630e44c0057c2b09e6ab5
Samuel André <samuel@multitecsistemas.com.br>
2021-03-25 13:34:57 -0300

    Aula 1

---
+++ src/boardgame/Board.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Board {
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		this.pieces = new Piece[rows][columns];
+	}
+
+	protected int getRows() {
+		return rows;
+	}
+
+	protected void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	protected int getColumns() {
+		return columns;
+	}
+
+	protected void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		return this.pieces[row][column];
+	}
+	public Piece piece(Position position) {
+		return this.pieces[position.getRow()][position.getColumn()];
+	}
+}
---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,15 @@
+package chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+	private Board board;
+	
+	public ChessMatch() {
+		this.board = new Board(8, 8);
+	}
+	
+	public ChessPiece[][] getPieces() {
+		return null;
+	}
+}
